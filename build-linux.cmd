eed2cf69a135745067885cb14b63923de2f97015
multios12 <5012052+multios12@users.noreply.github.com>
2022-01-16 02:58:49 +0900

    fix: css/jsの相対URL化

--- build-linux.cmd
+++ build-linux.cmd
@@ -2,8 +2,11 @@ echo off
 
 rem build front page
 call yarn build
+robocopy .\build .\srv\static /MIR
+rmdir /s /Q build
 
 rem build server app
+mkdir dist
 cd srv
 set GOOS=linux
 set GOARCH=amd64
